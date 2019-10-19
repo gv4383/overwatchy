5d399e9203f63689dc813776fc4a049b31ee5e2e
gv4383 <gregory.vargas18@gmail.com>
2019-10-19 18:40:57 -0500

    Added float transition to HeroCard component

--- src/components/HeroCard.vue
+++ src/components/HeroCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card float-card">
     <div class="card-image">
       <figure class="image is-4by3">
         <!-- <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"> -->
@@ -32,3 +32,36 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+$defaultDuration: .3s;
+
+@mixin hideTapHighlightColor() {
+  //Prevent highlight colour when element is tapped
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
+}
+
+@mixin hardwareAccel() {
+  //Improve performance on mobile/tablet devices
+  transform: translateZ(0);
+}
+
+@mixin improveAntiAlias() {
+  //Improve aliasing on mobile/tablet devices
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+}
+
+.float-card {
+  display: inline-block;
+  transition-duration: $defaultDuration;
+  transition-property: transform;
+
+  @include hideTapHighlightColor();
+  @include hardwareAccel();
+  @include improveAntiAlias();
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+}
+</style>
