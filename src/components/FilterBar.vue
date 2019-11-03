2bb038fd688417a052b0b18270ce76a57239cacc
gv4383 <gregory.vargas18@gmail.com>
2019-11-03 17:00:09 -0600

    Added checkboxes for filter options

--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -1,20 +1,39 @@
 <template>
-  <div>
-    <span v-on:click="test">All</span>
-    <span>Tank</span>
-    <span>Offense</span>
-    <span>Defense</span>
-    <span>Support</span>
+  <div class="center-options">
+    <b-checkbox v-model="checkboxGroup" native-value="tank">
+        Tank
+    </b-checkbox>
+    <b-checkbox v-model="checkboxGroup" native-value="offense">
+        Offense
+    </b-checkbox>
+    <b-checkbox v-model="checkboxGroup" native-value="defense">
+        Defense
+    </b-checkbox>
+    <b-checkbox v-model="checkboxGroup" native-value="support">
+        Support
+    </b-checkbox>
   </div>
 </template>
 
 <script>
 export default {
   name: 'FilterBar',
-  methods: {
-    test() {
-      console.log('TEST');
-    },
+  data() {
+    return {
+      checkboxGroup: [
+        'tank',
+        'offense',
+        'defense',
+        'support',
+      ],
+    };
   },
 };
 </script>
+
+<style lang="scss">
+.center-options {
+  display: flex;
+  justify-content: center;
+}
+</style>
